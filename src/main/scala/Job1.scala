53d018e23db04d3b35e8e09e33d43fbe14c1f32a
Georg Heiler <georg.kf.heiler@gmail.com>
2018-06-01 15:57:17 +0200

    trying new spark ORC support

--- src/main/scala/Job1.scala
+++ src/main/scala/Job1.scala
@@ -1,17 +1,30 @@
 import org.apache.spark.SparkConf
+import org.apache.spark.serializer.KryoSerializer
 import org.apache.spark.sql.SparkSession
 import org.geotools.data.DataStoreFinder
 import org.locationtech.geomesa.fs.storage.common.PartitionScheme
+import org.locationtech.geomesa.spark.GeoMesaSparkKryoRegistrator
 import org.locationtech.geomesa.spark.jts._
 import org.locationtech.geomesa.utils.interop.SimpleFeatureTypes
+
 import collection.JavaConversions._
 
 object Job1 extends App {
   val spark = SparkSession
     .builder()
     .config(new SparkConf().setAppName("dummy")
-      .setIfMissing("spark.master", "local[*]"))
-    .enableHiveSupport()
+      .setIfMissing("spark.master", "local[*]")
+      .setIfMissing("spark.sql.orc.impl", "native")
+      .setIfMissing("spark.sql.orc.enabled", "true")
+      .setIfMissing("park.sql.orc.enableVectorizedReader", "true")
+      .setIfMissing("spark.sql.hive.convertMetastoreOrc", "true")
+      .setIfMissing("spark.sql.orc.filterPushdown", "true")
+      .setIfMissing("spark.sql.orc.char.enabled", "true")
+      .setIfMissing("spark.kryo.registrator", classOf[GeoMesaSparkKryoRegistrator].getName)
+      .setIfMissing("spark.serializer", classOf[KryoSerializer].getCanonicalName)
+      .setIfMissing("spark.kryo.unsafe", "true")
+      .setIfMissing("spark.kryo.referenceTracking", "false")
+    )
     .getOrCreate()
 
   spark.withJTS
